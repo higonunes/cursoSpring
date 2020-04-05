f64371bbd4971fda42aab5c1db2e60ec5c165079
Higo Nunes <higo.sousaa@gmail.com>
2020-04-05 20:20:25 -0300

    Validando email

--- src/main/java/com/higo/learning/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/higo/learning/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.higo.learning.services.validation;
 
+import com.higo.learning.domain.Cliente;
 import com.higo.learning.dto.ClienteNewDTO;
 import com.higo.learning.enums.TipoCliente;
+import com.higo.learning.repositories.ClienteRepository;
 import com.higo.learning.resources.exception.FieldMessage;
 import com.higo.learning.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -12,6 +15,8 @@ import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClientInsert, ClienteNewDTO> {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @Override
     public void initialize(ClientInsert constraintAnnotation) {
@@ -27,6 +32,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClientInsert,
         }
         if (clienteNewDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(clienteNewDTO.getCpfOuCnpj())) {
             list.add(new FieldMessage("CpfOuCnpj", "O CNPJ não é valido"));
+        }
+
+        Cliente aux = clienteRepository.findByEmail(clienteNewDTO.getEmail());
+
+        if(aux != null) {
+            list.add(new FieldMessage("email", "Email já existente"));
         }
 
         for (FieldMessage e : list) {
