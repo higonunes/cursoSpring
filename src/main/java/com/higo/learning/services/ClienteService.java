98d91d0e24dc84093972a49caa9eec5f7c9aefa6
Higo Nunes <higo.sousaa@gmail.com>
2020-04-13 03:28:36 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/higo/learning/services/ClienteService.java
+++ src/main/java/com/higo/learning/services/ClienteService.java
@@ -15,6 +15,7 @@ import com.higo.learning.services.exceptions.AuthorizationException;
 import com.higo.learning.services.exceptions.DataIntegrityException;
 import com.higo.learning.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -23,6 +24,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -41,6 +43,12 @@ public class ClienteService {
 
     @Autowired
     private S3Service s3Service;
+
+    @Autowired
+    private ImageService imageService;
+
+    @Value("${img.prefix.client.profile}")
+    private String prefix;
 
     public Cliente find(Integer id) {
         UserSS user = UserService.authenticated();
@@ -111,12 +119,12 @@ public class ClienteService {
             throw new AuthorizationException("Acesso negado");
         }
 
-        URI uri = s3Service.uploadFile(multipartFile);
+        BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+        String fileName = prefix + user.getId() + ".jpg";
 
-        Optional<Cliente> obj = repo.findById(user.getId());
-        obj.get().setImageURL(uri.toString());
-        repo.save(obj.get());
-        return uri;
+
+        return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
+
     }
 
     private void updateData(Cliente newObj, Cliente obj) {
